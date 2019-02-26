5c11e8d56647bac9430dc3d36241ca85b195ad9a
yoan90 <38156178+yoan90@users.noreply.github.com>
2019-02-26 17:28:39 +0100

    Update SpecialOfferType.java

--- src/main/java/fr/esiea/supermarket/model/SpecialOfferType.java
+++ src/main/java/fr/esiea/supermarket/model/SpecialOfferType.java
@@ -1,5 +1,5 @@
 package dojo.supermarket.model;
 
 public enum SpecialOfferType {
-    ThreeForTwo, TenPercentDiscount, TwoForAmount, FiveForAmount, TwentyPercentDiscount;
+    ThreeForTwo, TenPercentDiscount, TwoForAmount, FiveForAmount;
 }
