e0c243890d711f7de56600bb6fc411353337711a
magi-esiea <38328874+magi-esiea@users.noreply.github.com>
2019-02-27 23:46:34 +0100

    Add ReceiptPrinter Test

--- src/test/java/fr/supermarket/model/SupermarketTest.java
+++ src/test/java/fr/supermarket/model/SupermarketTest.java
@@ -285,16 +285,52 @@ public class SupermarketTest {
         ReceiptPrinter printerDefault = new ReceiptPrinter();
 
         String expectedPrintDefault =
-        "toothbrush                          1.98\n"
+        "Toothbrush                          1.98\n"
         +"  0.99 * 2\n"
-        +"toothbrush                          0.99\n"
-        +"apples                              4.98\n"
+        +"Toothbrush                          0.99\n"
+        +"Apples                              4.98\n"
         +"  1.99 * 2.500\n"
         +"3 for 2(toothbrush)                -0.99\n"
         +"\n"
         +"Total:                              6.96";
         
         Assertions.assertThat(expectedPrintDefault).isEqualTo(printerDefault.printReceipt(receipt)).as("Default Receipt Printer");
+        
+    }
+    
+    
+    @Test
+    public void WithColsReceiptPrinterTest() {
+
+        SupermarketCatalog catalog = new FakeCatalog();
+
+        Product toothbrush = new Product("Toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("Apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothbrush, 2);
+        cart.addItemQuantity(toothbrush,1);
+        cart.addItemQuantity(apples, 2.5);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 1);
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+        ReceiptPrinter printerDefault = new ReceiptPrinter();
+        ReceiptPrinter printerWithCols = new ReceiptPrinter(30);
+
+        String expectedPrintWithCol =
+        "Toothbrush                1.98\n"
+        +"  0.99 * 2\n"
+        +"Toothbrush                0.99\n"
+        +"Apples                    4.98\n"
+        +"  1.99 * 2.500\n"
+        +"3 for 2(toothbrush)      -0.99\n"
+        +"\n"
+        +"Total:                    6.96";
+
+        Assertions.assertThat(expectedPrintWithCol).isEqualTo(printerWithCols.printReceipt(receipt)).as("Receipt printer loaded with 30 columns");
 
     }
      
